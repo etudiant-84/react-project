007d8b00116f13916c97b0d6416b2505cb81d131
zakarya <cherdoudi_zakarya@yahoo.fr>
2025-03-18 15:15:53 -0400

    fonfig react-project

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+     return(
+        <header>
+        <div className="profile">
+            <img src="/api/placeholder/100/100" alt="Profile Photo" class="profile-image" />
+            <div>
+                <h1>Jean Dupont</h1>
+                <p>Développeur mobile spécialisé en applications multiplateformes et UX/UI design</p>
+            </div>
+        </div>
+      </header>
+     )
+}
+
+export default Header;
